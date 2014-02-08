e2dc09b9dc87c46e4571d38177fd4bb94ad4bea4
Vaughan Hilts <hilts.vaughan@gmail.com>
2014-02-07 19:40:19 -0500

    Added a hashing class with unit tests
    * Fully tested MD5 and SHA512 hashing algorithims

---
+++ OpenORPG.Server.Tests/Infrastructure/Cryptography/HashTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server.Infrastructure.Cryptography;
+using Xunit;
+
+namespace OpenORPG.Server.Tests.Infrastructure.Cryptography
+{
+    /// <summary>
+    /// Provides a collection of tests for hashes to ensure they're working correctly.
+    /// </summary>
+    public class HashTests
+    {
+
+        /// <summary>
+        /// This test ensures the MD5 function is working correctly
+        /// </summary>
+        [Fact]
+        public void Md5ShouldReturnLowercaseMd5()
+        {
+            var testCase = HashHelper.GetMD5("thisissomething");
+            const string expectedTestCase = "b95b8b34ac2872a1192f803aa9dd92a8";
+
+            var testCase2 = HashHelper.GetMD5("thisisareallylongstring!@#44withalotofelements");
+            const string expectedTestCase2 = "e1060c0600512023069ec54f8de79708";
+
+            var testCase3 = HashHelper.GetMD5("");
+            const string expectedTestCase3 = "d41d8cd98f00b204e9800998ecf8427e";
+
+            Assert.Equal(expectedTestCase, testCase);
+            Assert.Equal(expectedTestCase2, testCase2);
+            Assert.Equal(expectedTestCase3, testCase3);
+        }
+
+        /// <summary>
+        /// This test ensures the Sha512 function is working correctly.
+        /// </summary>
+        [Fact]
+        public void Sha512ShouldReturnLowercaseMd5()
+        {
+            var testCase = HashHelper.GetSHA512("thisissomething");
+            var testCase2 = HashHelper.GetSHA512("thisisareallylongstring!@#44withalotofelements");
+            var testCase3 = HashHelper.GetSHA512("");
+
+            var expected =
+                "1A2860F4E7F6F74827185CFBF3B798D69119C4F19FFB63925DCAB92A0E38FA0C2F9FE8944B784625DFE3D718A2DFC6F057226F92F735F89D9B6EE91837AAD456"
+                    .ToLower();
+            var expected2 =
+                "D9984731A2C39BCDFA995DF6AF41040C3BAE584639EA361BF6B2F9736B617D8833532585871076BF6F200C294939788AF9AF8B98C7FD3D32F74E2F078039C92C"
+                    .ToLower();
+            var expected3 =
+                "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"
+                    .ToLower();
+
+            Assert.Equal(expected, testCase);
+            Assert.Equal(expected2, testCase2);
+            Assert.Equal(expected3, testCase3);
+        }
+
+
+    }
+}
---
+++ OpenORPG.Server/Infrastructure/Cryptography/HashHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Infrastructure.Cryptography
+{
+    public class HashHelper
+    {
+
+        public enum HashType
+        {
+            SHA512,
+            MD5
+        }
+
+        /// <summary>
+        /// Gets the SHA512 of a mesage and returns it in all uppercase form
+        /// </summary>
+        /// <param name="message">The message to get</param>
+        /// <returns></returns>
+        public static string GetSHA512(string message)
+        {
+            return ComputeHash(message, HashType.SHA512);
+        }
+
+        /// <summary>
+        /// Gets the MD5 of a given string and returns it in all lowercase form.
+        /// </summary>
+        /// <param name="message">The message to get</param>
+        /// <returns></returns>
+        public static string GetMD5(string message)
+        {
+            return ComputeHash(message, HashType.MD5);
+        }
+
+
+        /// <summary>
+        /// A small helper function that given a hash type will use the .NET libraries to compute and return the proper hash.
+        /// </summary>
+        /// <param name="message">The message to hash</param>
+        /// <param name="type">The type of hash to compute</param>
+        /// <returns></returns>
+        private static string ComputeHash(string message, HashType type)
+        {
+            byte[] sourceBytes = Encoding.Default.GetBytes(message);
+            byte[] hashBytes = null;
+
+            switch (type)
+            {
+                case HashType.MD5:
+                    hashBytes = MD5.Create().ComputeHash(sourceBytes);
+                    break;
+                case HashType.SHA512:
+                    hashBytes = SHA512.Create().ComputeHash(sourceBytes);
+                    break;
+                default:
+                    throw new NotImplementedException("The given hash type is not implemented");
+            }
+            
+            var sb = new StringBuilder();
+            
+            foreach (byte t in hashBytes)
+                sb.AppendFormat("{0:x2}", t);
+
+            return sb.ToString().ToLower();
+        }
+
+
+
+    }
+}
