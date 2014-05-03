9a44412dc0360d98e7bc238bf9e7cfda8b71a0b5
Vaughan Hilts <hilts.vaughan@gmail.com>
2014-05-03 15:34:43 -0400

    Added some basic NPC classes
    * Getting started on client UI soon

---
+++ OpenORPG.Server/Game/Entities/Npc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Game.Entities
+{
+    
+    /// <summary>
+    /// An NPC is an object in the game world that can be 
+    /// </summary>
+    public class Npc
+    {
+    }
+}
