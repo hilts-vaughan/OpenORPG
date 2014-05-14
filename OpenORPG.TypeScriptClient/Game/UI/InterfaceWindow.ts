363ea9c859f69c899f05a855bd020b4eff415730
Vaughan Hilts <hilts.vaughan@gmail.com>
2014-05-13 22:34:13 -0400

    Random tooltip stuff for inventory
    * Needs some server-side wiring

--- OpenORPG.TypeScriptClient/Game/UI/InterfaceWindow.ts
+++ OpenORPG.TypeScriptClient/Game/UI/InterfaceWindow.ts
@@ -5,7 +5,7 @@
      */
     export class InterfaceWindow {
 
-        private windowName: string;
+        public windowName: string;
 
         constructor(windowFile: string, windowName: string) {
 
@@ -25,6 +25,7 @@
             // Get jQuery to load the static content into the page
             $.get(windowFile, data => {
                 $(windowName).html(data);
+                this.ready();
             });
 
             this.windowName = windowName;
@@ -38,7 +39,9 @@
             $(this.windowName).dialog("close");
         }
 
+        ready() {
 
+        }
 
     }
 
@@ -48,7 +51,54 @@
         // Create our inventory window
         constructor() {
             super("assets/hud/inventory.html", "#inventorydialog");
+
+
         }
+
+        ready() {
+
+            // Setup drag events
+            $(".item").draggable({ revert: 'invalid' });
+
+            $('.itemslot').droppable({
+                accept: '.item',
+
+                drop: function (ev, ui) {
+                    var dropped = ui.draggable;
+                    var droppedOn = $(this);
+                    $(dropped).detach().css({ top: 0, left: 0 }).appendTo(droppedOn);
+                }
+
+            });
+
+            $(this.windowName).tooltip({
+                position: {
+                    my: "center bottom-20",
+                    at: "center top",
+                    using: function (position, feedback) {
+                        $(this).css(position);
+                        $("<div>")
+                            .addClass("arrow")
+                            .addClass(feedback.vertical)
+                            .addClass(feedback.horizontal)
+                            .addClass("inventorytooltip")
+                            .appendTo(this);
+                    }
+                }
+            });
+
+
+
+        }
+
+        itemInSpot(drag_item, spot) {
+            var item = $('<div />'); // create new img element
+            item.attr({ // copy attributes
+                src: drag_item.attr('src'),
+            }).attr('class', drag_item.attr('class')).appendTo(spot).draggable({ revert: 'invalid' }); // add to spot + make draggable
+            drag_item.remove(); // remove the old object
+        }
+
 
     }
 
