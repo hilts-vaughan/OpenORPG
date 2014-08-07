3e481b6163599c75a284511ca906db17f3b81a4a
Vaughan Hilts <hilts.vaughan@gmail.com>
2014-08-07 00:01:13 -0400

    Added new controllers for skills and monsters
    * These are used on the editor endpoints to allow editing and
    persistance

---
+++ OpenORPG.Editor/Server/Controllers/MonsterController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Inspire.Shared.Models.Enums;
+using OpenORPG.Database.DAL;
+using Server.Game.Database;
+using Server.Game.Database.Models.ContentTemplates;
+
+namespace OpenORPG.Editor.Server.Controllers
+{
+
+    [RoutePrefix("api/monsterss")]
+    public class MonsterController : ApiController
+    {
+        [HttpGet]
+        [Route("{id:int}")]
+        public ItemTemplate GetMonsterById(int id)
+        {
+
+            using (var context = new GameDatabaseContext())
+            {
+                var repo = new MonsterRepository(context);
+                repo.Get(id);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<ItemTemplate> GetMonsters()
+        {
+            using (var context = new GameDatabaseContext())
+            {
+                var repo = new MonsterRepository(context);
+                repo.GetAll();
+            }
+        }
+
+        [HttpPost]
+        [Route("{id:int}")]
+        public IHttpActionResult SaveMonster(MonsterTemplate monsterTemplate, int id)
+        {
+            using (var context = new GameDatabaseContext())
+            {
+
+                var repo = new MonsterRepository(context);
+                repo.Update(monsterTemplate, id);
+
+                return Ok();
+            }
+
+
+        }
+
+        [HttpPut]
+        [Route("")]
+        public IHttpActionResult NewMonster()
+        {
+            using (var context = new GameDatabaseContext())
+            {
+
+                var repo = new MonsterRepository(context);
+                var skill =
+                    repo.Add(new MonsterTemplate());
+
+                return Ok(skill);
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ OpenORPG.Editor/Server/Controllers/SkillController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Inspire.Shared.Models.Enums;
+using OpenORPG.Database.DAL;
+using Server.Game.Database;
+using Server.Game.Database.Models.ContentTemplates;
+
+namespace OpenORPG.Editor.Server.Controllers
+{
+
+    [RoutePrefix("api/skills")]
+    public class SkillController : ApiController
+    {
+        [HttpGet]
+        [Route("{id:int}")]
+        public ItemTemplate GetSkillById(int id)
+        {
+
+            using (var context = new GameDatabaseContext())
+            {
+                var repo = new SkillRepository(context);
+                repo.Get(id);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<ItemTemplate> GetSkills()
+        {
+            using (var context = new GameDatabaseContext())
+            {
+                var repo = new SkillRepository(context);
+                repo.GetAll();
+            }
+        }
+
+        [HttpPost]
+        [Route("{id:int}")]
+        public IHttpActionResult SaveSkill(SkillTemplate skillTemplate, int id)
+        {
+            using (var context = new GameDatabaseContext())
+            {
+
+                var repo = new SkillRepository(context);
+                repo.Update(skillTemplate, id);
+
+                return Ok();
+            }
+
+
+        }
+
+        [HttpPut]
+        [Route("")]
+        public IHttpActionResult NewSkill()
+        {
+            using (var context = new GameDatabaseContext())
+            {
+
+                var repo = new SkillRepository(context);
+                var skill =
+                    repo.Add(new SkillTemplate(SkillType.Damage, SkillTargetType.Enemy, SkillActivationType.Immediate, 0,
+                        0, "", 0, "New Skill"));
+
+                return Ok(skill);
+            }
+
+
+        }
+
+
+
+    }
+}
