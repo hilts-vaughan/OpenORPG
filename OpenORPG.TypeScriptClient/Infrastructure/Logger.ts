3e38a90f2d03952aa2a22ed89364a453838ed14d
Vaughan Hilts <hilts.vaughan@gmail.com>
2014-08-13 23:17:58 -0400

    Added some more configuration to the actual logging framework

--- OpenORPG.TypeScriptClient/Infrastructure/Logger.ts
+++ OpenORPG.TypeScriptClient/Infrastructure/Logger.ts
@@ -3,12 +3,14 @@
      var log4javascript: any = window["log4javascript"].getLogger();
      var log = log4javascript;
 
+     log.setLevel(window["log4javascript"]["Level"]["ALL"]);        
      var consoleLogger = new window["log4javascript"]["BrowserConsoleAppender"]();
      var popUpLayout = new window["log4javascript"]["PatternLayout"]("%d{HH:mm:ss} %-5p - %m%n");
      consoleLogger.setLayout(popUpLayout);
+     consoleLogger.setThreshold(window["log4javascript"]["Level"]["TRACE"]);
      log.addAppender(consoleLogger);
 
-     log.debug("Logging system has been booted up succesfully.");
+     log.debug("Logging system has been booted up succesfully");
 
      export function trace(...params) {
          log.trace(params);
