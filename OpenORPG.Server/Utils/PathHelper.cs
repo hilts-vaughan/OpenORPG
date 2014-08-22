b8ed6646fc67d5075cbe5d2de5641f25ac32a3bc
Vaughan Hilts <hilts.vaughan@gmail.com>
2014-08-21 20:58:22 -0400

    Revert "DB working"
    This reverts commit a48221f723fc595ac157e370d7f12bd80d31814b.

--- OpenORPG.Server/Utils/PathHelper.cs
+++ OpenORPG.Server/Utils/PathHelper.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-
-namespace Server.Utils
+﻿namespace Server.Utils
 {
     /// <summary>
     ///     Maintains paths for certain assets for making seeking to certain directories easier and less hardcoded.
@@ -11,32 +9,36 @@ namespace Server.Utils
         ///     This is the main asset path; it isn't advised to reference this directly as it will create nasty hard coded links
         ///     //TODO: We should set the server to copy its own files at some point but for now using the clients will suffice
         /// </summary>
-        public static readonly string AssetBasePath =  "..\\..\\..\\..\\OpenORPG.TypeScriptClient\\assets\\";
+        public const string AssetBasePath = "..\\..\\..\\..\\OpenORPG.TypeScriptClient\\assets\\";
 
 
         // These paths are not releative to the AssetBasePath because the content loader already assumes the base asset path as the working directory
-        static PathHelper()
-        {
-            AssetBasePath = Path.Combine("..", "..", "..", "..", "OpenORPG.TypeScriptClient", "assets");
-        }
+
 
         public static string MapExtension
         {
             get { return ".TMX";  }
         }
 
- 
+        /// <summary>
+        ///     A path to the attributes folder
+        /// </summary>
+        public static string AttributesPath
+        {
+            get { return "Attributes\\"; }
+        }
+
         /// <summary>
         ///     A path to the maps folder
         /// </summary>
         public static string MapPath
         {
-            get { return "Maps"; }
+            get { return "Maps\\"; }
         }
 
         public static string SpritesPath
         {
-            get { return "sprites";  }
+            get { return "sprites\\";  }
         }
     }
 }
