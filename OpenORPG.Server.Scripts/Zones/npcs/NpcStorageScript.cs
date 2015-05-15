79711bed16e063a0e94308888dd6f51b4b898ee1
Vaughan Hilts <hilts.vaughan@gmail.com>
2015-05-14 20:40:57 -0400

    Added NPC storage scripts

---
+++ OpenORPG.Server.Scripts/Zones/npcs/NpcStorageScript.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenORPG.Server.Scripts.Zones.Npcs
+{
+    /// <summary>
+    /// This is a generic demo script provided that allows
+    /// </summary>
+    public class NpcStorageScript
+    {
+    }
+}
