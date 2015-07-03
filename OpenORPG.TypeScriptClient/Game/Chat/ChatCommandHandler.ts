615628229386623d70914a494cfa506ff4813fd3
Vaughan Hilts <vhilts@axonify.com>
2015-07-03 18:06:31 -0400

    Documented the ChatCommandHandler better so future developers will understand the parameters

--- OpenORPG.TypeScriptClient/Game/Chat/ChatCommandHandler.ts
+++ OpenORPG.TypeScriptClient/Game/Chat/ChatCommandHandler.ts
@@ -11,10 +11,6 @@
          
          private callbackTable = {};
 
-         constructor() {
-             
-         }
-
          /*
           * Allows registration of callbacks from the outside world. The chat manager will
           * be responsible for matching and executing these. Callbacks will be fired accordingly.
@@ -24,21 +20,31 @@
          }
 
          /*
-          * Handles the incoming command type and parses them on the client sided of things.
+          * Handles the incoming command type and attempts to fire the callback from the table.
+            If it cannot be found, then it is ignored.
           */
-         handleCommand(command: CommandType, message: string) {            
-             // Get the arguments to pass in here
+         handleCommand(command: CommandType, message: string) {                         
              var args = this.getArgumentsFromMessage(message);
-             this.callbackTable[command](args);
+             if(this.callbackTable[command]) {
+                this.callbackTable[command](args);
+             }
          }
-
+         
+         /**
+          * Given a string from the user input, returns the parameters from them.
+          * Parameters are identified by spaces, unless they are double quoted. For example:
+          * 
+          * 'Vaughan Apple Banana' would be three parameters, ['Vaughan', 'Apple', 'Banana']
+          * 'Vaughan "Apple Banana"' would be two parameters, ['Vaughan', 'Apple Banana']
+          * 
+          * The latter is primairly used for looking up user names and the like, things with
+          * spaces in them. Generally, parameters are seperated by spaces.         
+          */
          private getArgumentsFromMessage(message: string) : Array<string> {
              var args = message.match(/(?:[^\s"]+|"[^"]*")+/g);
              args.shift();
              return args;
          }
-
-
      }
 
  }
