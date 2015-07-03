19c20a5db3a4eb40c714bc60dd7a4b6c7e8dac11
Vaughan Hilts <vhilts@axonify.com>
2015-07-03 17:58:40 -0400

    Removed some TODOs that are now completed

--- OpenORPG.TypeScriptClient/Infrastructure/Localization/LocaleManager.ts
+++ OpenORPG.TypeScriptClient/Infrastructure/Localization/LocaleManager.ts
@@ -1,38 +1,46 @@
 ﻿module OpenORPG {
-
+    
+    /**
+     * Provides localization services to the entire game application. 
+     */
     export class LocaleManager {
 
         private static _instance: LocaleManager = null;
-        private locale : string;
+        private _localeCode : string;
         private currentLocaleData : Object;
 
         constructor(locale: string) {
-            LocaleManager._instance = this;
-
-            // Set the locale
+            LocaleManager._instance = this;            
             this.setLocale(locale);            
         }
-
+        
+        /**
+         * Given a dot delimited message type, applies arguments to it and returns the localized version of it
+         * for the current locale code.
+         */
         public getString(messageType: string, args : Array<string> ) {
             var str = this.currentLocaleData[messageType];
-
-            if (str) {
-
-                // If the arguments are presented, use them
+            if (str) {                
                 if (args) {
                     for (var i = 0; i < args.length; i++)
                         str = str.split("{" + i + "}").join(args[i]);
                 }
-
             } else {
-                str = "Error! No translation found for message type of " + messageType;
+                str = "Error! No translation found for message type of " + messageType + " given locale code of " + this._localeCode;
             }
-
             return str;
         }
 
-        public setLocale(locale : string) {
-            $.getJSON("assets/locale/" + locale + ".json", (data) => {
+        /**
+         * Sets the locale of the manager based on a locale code. Supported locale codes are:
+         * EN: English
+         * FR: French
+         * JP: Japanese
+         * DV: Development 
+         */
+        public setLocale(localeCode : string) {
+            this._localeCode = localeCode;
+            $.getJSON("assets/locale/" + localeCode + ".json", (data) => {
                 this.currentLocaleData = data;
             });
         }
@@ -43,8 +51,6 @@
             }
             return LocaleManager._instance;
         }
-
-
 
     }
 }
